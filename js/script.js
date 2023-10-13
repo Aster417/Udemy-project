d8d44dc9bce39b558d66583fcafe913c63a2f8d0
Aster417 <tmith417@gmail.com>
2023-10-13 17:21:24 +0300

    d
    d

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log(1);
+"use strict";
+
+
+console.log("");
+
+const person = `Alex`;
+
+const bool = true;
+
+let und;
+console.log("und");
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+console.log(obj.name);
+
+let arr = ["plum.png", "orange.jpg", 6, {}, []];
+
+console.log(arr[1]);
